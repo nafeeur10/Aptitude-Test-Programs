f2d58529b911c401a4a392a93bcdbfaf4d601d3d
nafeeur10 <nafeeur.kuet.cse6@gmail.com>
2018-10-18 22:21:15 +0600

    Pyramid

---
+++ Pyramid.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int i, j, k;
+    for(i=1; i<=n; i++)
+    {
+        for(j=0; j<(n-i); j++)
+        {
+            cout<<" ";
+        }
+
+        for(k=0; k<i; k++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
